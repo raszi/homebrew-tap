76ddedae64b054730b21a4bfcd4c27e5dfac9db4
KARASZI István <github@spam.raszi.hu>
2017-06-06 19:00:35 +0200

    Update kube-helpers

--- kube-helpers.rb
+++ kube-helpers.rb
@@ -1,8 +1,8 @@
 class KubeHelpers < Formula
   desc 'Kubernetes Helper Tools'
   homepage 'https://github.com/raszi/kube-helpers'
-  url 'https://github.com/raszi/kube-helpers/archive/v0.0.2.tar.gz'
-  sha256 'bf5a773c64c00fb78c28a3649401846c8a7b5c285617880bec3c54bffc43c276'
+  url 'https://github.com/raszi/kube-helpers/archive/v0.0.3.tar.gz'
+  sha256 '2c327eb42abaf8f87257ea607511856ef169df8312a95237090adf49c37fe7d8'
 
   def install
     bin.install 'k-proxy'
