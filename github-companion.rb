5359e2ba948da03d8cd81c0ff3cf9c07acb1d7da
KARASZI István <github@spam.raszi.hu>
2018-04-17 16:16:06 +0200

    Update github-companion to v0.4.0

--- github-companion.rb
+++ github-companion.rb
@@ -1,9 +1,9 @@
 class GithubCompanion < Formula
   desc "Small tool to interact with GitHub API"
   homepage "https://github.com/whitepages/github-companion"
-  url "https://github.com/whitepages/github-companion/releases/download/v0.3.0/github-companion-0.3.0.jar",
+  url "https://github.com/whitepages/github-companion/releases/download/v0.4.0/github-companion-0.4.0.jar",
       :using => :nounzip
-  sha256 "5842a4a83fdcba85cfebc6af5d65e7eb6f08c786ef8e1b660987471e0e300df1"
+  sha256 "9014ada732313ffcd5dc71b57362c093322658a7e053af3c4534cef97e78bfbb"
 
   bottle :unneeded
   depends_on :java
@@ -12,7 +12,7 @@ class GithubCompanion < Formula
 
   def install
     bin.install "github-companion"
-    pkgshare.install "github-companion-0.3.0.jar"
+    pkgshare.install "github-companion-0.4.0.jar"
   end
 end
 
@@ -22,15 +22,14 @@ new file mode 100644
 index 0000000..5a91c75
 --- /dev/null
 +++ b/github-companion
-@@ -0,0 +1,11 @@
+@@ -0,0 +1,10 @@
 +#!/bin/bash
 +
 +java \
 +    -client \
 +    -XX:+TieredCompilation \
 +    -XX:TieredStopAtLevel=1 \
-+    -XX:+UseConcMarkSweepGC \
 +    -XX:+CMSClassUnloadingEnabled \
 +    -Xverify:none \
-+    -jar "$(brew --cellar)/github-companion/0.3.0/share/github-companion/github-companion-0.3.0.jar" \
++    -jar "$(brew --cellar)/github-companion/0.4.0/share/github-companion/github-companion-0.4.0.jar" \
 +    "$@"
