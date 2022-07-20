ebe9200258a372d1deb6d328009ec37288d15cfb
KARASZI István <ikaraszi@gmail.com>
2022-07-20 18:09:21 +0200

    Replace apostrophes with quotes

--- kube-helpers.rb
+++ kube-helpers.rb
@@ -1,14 +1,14 @@
 class KubeHelpers < Formula
-  desc 'Kubernetes Helper Tools'
-  homepage 'https://github.com/raszi/kube-helpers'
-  url 'https://github.com/raszi/kube-helpers/archive/v0.0.3.tar.gz'
-  sha256 '2c327eb42abaf8f87257ea607511856ef169df8312a95237090adf49c37fe7d8'
+  desc "Kubernetes Helper Tools"
+  homepage "https://github.com/raszi/kube-helpers"
+  url "https://github.com/raszi/kube-helpers/archive/v0.0.3.tar.gz"
+  sha256 "2c327eb42abaf8f87257ea607511856ef169df8312a95237090adf49c37fe7d8"
 
   def install
-    bin.install 'k-proxy'
+    bin.install "k-proxy"
   end
 
   test do
-    system 'false'
+    system "false"
   end
 end
