de730412b96c3b41a573d1d67fc400b2bfcaf219
KARASZI István <ikaraszi@gmail.com>
2024-01-02 22:11:53 +0100

    Update cljfmt

--- cljfmt.rb
+++ cljfmt.rb
@@ -1,8 +1,8 @@
 class Cljfmt < Formula
   desc "Tool for detecting and fixing formatting errors in Clojure code."
   homepage "https://github.com/weavejester/cljfmt"
-  url "https://github.com/weavejester/cljfmt/releases/download/0.11.2/cljfmt-0.11.2-darwin-aarch64.tar.gz"
-  sha256 "ac9de3e0835f099dc839f2aa69468be417b774902c4062f371237ee3791865a0"
+  url "https://github.com/weavejester/cljfmt/releases/download/0.12.0/cljfmt-0.12.0-darwin-aarch64.tar.gz"
+  sha256 "24b857a87991edce2f331f64598e76e5dd23df7a3484bbda3a45283cedeab414"
   head "git@github.com:weavejester/cljfmt.git"
 
   def install
