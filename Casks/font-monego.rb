6094016777ef3c25897ac7dd18aa3d2cd61afc52
KARASZI István <ikaraszi@gmail.com>
2024-05-29 09:27:07 +0200

    Add Monego font

---
+++ Casks/font-monego.rb
@@ -0,0 +1,14 @@
+cask "font-monego" do
+  version "0.0.1"
+  sha256 "c48666511162e6d96b122c1b300940e94cb4c7a4a0c49eb7e9f23904f1facbd7"
+
+  url "https://github.com/cseelus/monego/archive/ad2acdcfc48277dabdededd3a46f1348f720d110.zip"
+  name "Monego font"
+  desc "The beloved Monaco monospaced font, recreated with bold and italic variants"
+  homepage "https://github.com/cseelus/monego"
+
+  font "monego-ad2acdcfc48277dabdededd3a46f1348f720d110/Monego/Monego-Bold.otf"
+  font "monego-ad2acdcfc48277dabdededd3a46f1348f720d110/Monego/Monego-BoldItalic.otf"
+  font "monego-ad2acdcfc48277dabdededd3a46f1348f720d110/Monego/Monego-Italic.otf"
+  font "monego-ad2acdcfc48277dabdededd3a46f1348f720d110/Monego/Monego-Regular.otf"
+end
