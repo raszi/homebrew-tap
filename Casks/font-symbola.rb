3374884bc16bc9a1da6d146ddf694a901d05d287
KARASZI István <ikaraszi@gmail.com>
2026-02-11 09:35:43 +0100

    Add font-symbola

---
+++ Casks/font-symbola.rb
@@ -0,0 +1,11 @@
+cask "font-symbola" do
+  version "0.0.1"
+  sha256 "fb989c065c830734dbb05a2a23d2ac61634f9bf47017668b2d61e2c58b74d0b9"
+
+  url "https://github.com/ChiefMikeK/ttf-symbola/archive/refs/heads/master.zip"
+  name "Symbola font"
+  desc "Font for unicode symbols part of"
+  homepage "https://github.com/ChiefMikeK/ttf-symbola"
+
+  font "ttf-symbola-master/Symbola-13.otf"
+end
